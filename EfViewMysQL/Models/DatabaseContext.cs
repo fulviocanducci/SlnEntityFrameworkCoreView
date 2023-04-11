9524e4322a90fa210b11a7e3317857d27b27254a
Fulvio Cezar Canducci Dias <fulviocanducci@hotmail.com>
2023-04-11 16:46:04 -0300

    Add project files.

---
+++ EfViewMysQL/Models/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EfViewMysQL.Models
+{
+   public sealed class DatabaseContext : DbContext
+   {
+      public DatabaseContext() { }
+
+      public DatabaseContext(DbContextOptions options) : base(options) { }
+
+      public DbSet<People> People { get; set; }
+
+      protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+      {
+         optionsBuilder.UseMySql
+            (
+               "server=localhost;user=root;password=senha;database=ef",
+               new MySqlServerVersion(new Version(8, 0, 31))
+            );
+      }
+   }
+}
---
+++ EfViewMysQL/Models/Mappings/PeopleMapping.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfViewMysQL.Models.Mappings
+{
+   public sealed class PeopleMapping : IEntityTypeConfiguration<People>
+   {
+      public void Configure(EntityTypeBuilder<People> builder)
+      {
+         builder.ToTable("peoples");
+         builder.HasKey(x => x.Id);
+         builder.Property(x => x.Id)
+            .UseMySqlIdentityColumn()
+            .HasColumnName("id");
+
+         builder.Property(x => x.Name)
+            .HasColumnName("name")
+            .HasMaxLength(100)
+            .IsRequired();
+
+         builder.Property(x => x.Sex)
+            .HasColumnName("sex")
+            .HasConversion<uint>();         
+      }
+   }
+}
